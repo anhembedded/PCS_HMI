1e862a5fd9fbb17e090a506fbeed676ba69548f6
Nathan Embedded <anh.embedded@gmail.com>
2023-06-25 15:25:02 +0700

    SEGGER BUG data overfloat

--- HMI_FPAC_BACKEND/Middlewares/SEGGER/Config/SEGGER_RTT_Conf.h
+++ HMI_FPAC_BACKEND/Middlewares/SEGGER/Config/SEGGER_RTT_Conf.h
@@ -91,7 +91,7 @@ Revision: $Rev: 24316 $
 #endif
 
 #ifndef   BUFFER_SIZE_UP
-  #define BUFFER_SIZE_UP                            (1024*8)  // Size of the buffer for terminal output of target, up to host (Default: 1k)
+  #define BUFFER_SIZE_UP                            (8192)  // Size of the buffer for terminal output of target, up to host (Default: 1k)
 #endif
 
 #ifndef   BUFFER_SIZE_DOWN
@@ -99,7 +99,7 @@ Revision: $Rev: 24316 $
 #endif
 
 #ifndef   SEGGER_RTT_PRINTF_BUFFER_SIZE
-  #define SEGGER_RTT_PRINTF_BUFFER_SIZE             (64u)    // Size of buffer for RTT printf to bulk-send chars via RTT     (Default: 64)
+  #define SEGGER_RTT_PRINTF_BUFFER_SIZE             (8192)    // Size of buffer for RTT printf to bulk-send chars via RTT     (Default: 64)
 #endif
 
 #ifndef   SEGGER_RTT_MODE_DEFAULT
